e8a7ad7ae909725ddb577f8a2e029d2aaf4d476c
Shlomi Borovitz <shlomib@altostra.com>
2023-01-29 20:05:37 +0200

    Skeleton

---
+++ functions/installation-status.js
@@ -0,0 +1,9 @@
+module.exports.handler = async (event, context) => {
+  return {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: {},
+    multiValueHeaders: {},
+    body: ""
+  }
+}
