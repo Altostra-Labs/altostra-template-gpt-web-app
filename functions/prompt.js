8d107b4249d09cd553d5d4acf16663c981f904f5
Shlomi Borovitz <shlomib@altostra.com>
2023-01-30 12:44:02 +0200

    Some code

--- functions/prompt.js
+++ functions/prompt.js
@@ -1,9 +1,51 @@
-module.exports.handler = async (event, context) => {
+const { SSM } = require('aws-sdk')
+const {
+  GPT_API_KEY_ENV,
+  request,
+} = require('./utils')
+
+const GPT_API_PARAM_NAME = process.env[GPT_API_KEY_ENV]
+const gptModel = process.env.GPT_MODEL
+
+exports.handler = async (event) => {
+  const prompt = event.body.prompt
+
+  console.log('Prompt: ', prompt)
+
+  const ssm = new SSM()
+  await ssm.putParameter({
+    Name: GPT_API_PARAM_NAME,
+    Value: apiKey,
+    Type: 'SecureString',
+  }).promise()
+
+  const { Parameter: {
+    Value: apiKey,
+  } } = await ssm.getParameter({
+    Name: GPT_API_PARAM_NAME,
+    WithDecryption: true,
+  }).promise()
+
+  console.log('Got API KEY: ', apiKey.replace(/./g, '*'))
+
+  const { data } = await request(
+    'https://api.openai.com/v1/completions', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+    data: {
+      model: gptModel,
+      prompt,
+    }
+  })
+
+  console.log('Got response', { data })
+
   return {
-    isBase64Encoded: false,
     statusCode: 200,
-    headers: {},
-    multiValueHeaders: {},
-    body: ""
+    body: {
+      text: data.choices?.[0]?.text,
+    }
   }
 }
--- functions/utils.js
+++ functions/utils.js
@@ -1,3 +1,44 @@
+const { request } = require('https')
+
 module.exports = {
-  GPT_API_KEY_ENV: 'GPT_API_KEY_PARAM'
+  GPT_API_KEY_ENV: 'GPT_API_KEY_PARAM',
+
+  request(url, { data, ...options }) {
+    return new Promise((resolve, reject) => {
+      const req = request(options, res => {
+        const data = []
+
+        res
+          .on('error', reject)
+          .on('data', chunk => data.push(chunk.toString('utf-8')))
+          .on('end', () => {
+            const result = {
+              response: res,
+              data: data.join(''),
+            }
+
+            if (
+              typeof res.statusCode !== 'number' ||
+              res.statusCode >= 400
+            ) {
+              reject(result)
+            }
+            else {
+              resolve(result)
+            }
+          })
+      })
+        .on('error', reject)
+
+      if (data !== undefined) {
+        if (typeof data !== 'string') {
+          data = JSON.stringify(data)
+        }
+
+        req.write(data, 'utf-8')
+      }
+
+      req.end()
+    })
+  }
 }
